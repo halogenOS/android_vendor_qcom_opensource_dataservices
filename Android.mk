7acebc3e038bd4fd4aa42d9f9e5a20980e3535cc
Simao Gomes Viana <xdevs23@outlook.com>
2017-10-08 12:13:04 +0200

    Allow building dataservices using flag
    Change-Id: I12f1643c6124b613410651a11d8b25e5b18a8e13

--- Android.mk
+++ Android.mk
@@ -1,3 +1,7 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-dataservices))
 include $(call all-subdir-makefiles)
+else
+ifeq ($(BUILD_CAF_DATASERVICES),true)
+include $(call all-subdir-makefiles)
+endif
 endif
